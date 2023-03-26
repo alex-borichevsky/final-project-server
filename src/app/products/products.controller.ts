5e6567db1aafb2c40dec049718707a903a0b87f0
Алексей <borichevskiy03@mail.ru>
2023-03-26 14:51:14 +0300

    refactor product & category controller

--- src/app/products/products.controller.ts
+++ src/app/products/products.controller.ts
@@ -13,15 +13,11 @@ import { Delete, Get, Param, Put, UseGuards } from '@nestjs/common/decorators';
 import { JwtPermissionsGuard } from '../security/guards/jwt-permissions.guard';
 import { RequirePermissions } from '../security/decorators/permissions.decorator';
 import { UserPermissions } from '../roles/enums/user-permissions.enum';
-import { CreateCategoryDto } from '../categories/dtos/create-category.dto';
 import { ApiBody } from '@nestjs/swagger';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger/dist';
-import { ProductsEntity } from './entities/products.entity';
-import { DeleteResult, UpdateResult } from 'typeorm';
 
 @ApiTags('products')
 @Controller('products')
-@UseGuards(JwtPermissionsGuard)
 export class ProductsController {
   constructor(private readonly productService: ProductsService) {}
 
@@ -34,6 +30,7 @@ export class ProductsController {
   })
   @UseInterceptors(FileInterceptor('image'))
   @Post()
+  @UseGuards(JwtPermissionsGuard)
   @RequirePermissions(UserPermissions.CreateProduct)
   async createProduct(@Body() body: CreateProductDto, @UploadedFile() image) {
     return await this.productService.createProduct(body, image);
@@ -46,7 +43,6 @@ export class ProductsController {
     type: CreateProductDto
   })
   @Get(':id')
-  @RequirePermissions(UserPermissions.GetProductById)
   getProductById(@Param('id') id: string) {
     return this.productService.getProductById(id);
   }
@@ -59,7 +55,6 @@ export class ProductsController {
     isArray: true
   })
   @Get()
-  @RequirePermissions(UserPermissions.GetAllProducts)
   getAllProducts() {
     return this.productService.getAllProducts();
   }
@@ -71,6 +66,7 @@ export class ProductsController {
     type: CreateProductDto
   })
   @Delete(':id')
+  @UseGuards(JwtPermissionsGuard)
   @RequirePermissions(UserPermissions.DeleteProduct)
   deleteProduct(@Param('id') id: string) {
     return this.productService.delete(id);
@@ -84,6 +80,7 @@ export class ProductsController {
     type: CreateProductDto,
   })
   @Put(':id')
+  @UseGuards(JwtPermissionsGuard)
   @RequirePermissions(UserPermissions.UpdateProduct)
   updateProduct(@Param('id') id: string, @Body() dto: CreateProductDto) {
     return this.productService.updateProduct(id, dto);

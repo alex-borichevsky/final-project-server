372fb96eb8109f22bdefa10aab5c4f2be5effee0
Irina-Belyavskaya <ir200210b@mail.ru>
2023-03-31 17:30:30 +0300

    fixed bugs

--- src/app/categories/entities/category.entity.ts
+++ src/app/categories/entities/category.entity.ts
@@ -12,6 +12,12 @@ export class CategoryEntity extends IDEntity{
   @Column({name: 'description'})
   description: string;
 
+  @Column({
+    name: 'image_url', 
+    default: 'https://res.cloudinary.com/dzouilf8r/image/upload/v1680113277/photo-1631679706909-1844bbd07221_qkpg37.avif'
+  })
+  image: string;
+
   @OneToMany(() => ProductsEntity, product => product.category)
   products?: ProductsEntity[];
 
